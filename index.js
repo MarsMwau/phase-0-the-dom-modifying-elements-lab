7a02b89d6b91520a68486747361e9a3f4f1fb249
MarsMwau <marthamumbua16@gmail.com>
2023-03-22 15:32:47 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const body = document.getElementsByTagName("body")[0];
+const firstChild = body.querySelector("main:nth-child(1)");
+body.removeChild(firstChild);
+
+const newHeader = document.createElement("h1");
+newHeader.id="victory"
+newHeader.textContent= "Martha is the champion";
+
